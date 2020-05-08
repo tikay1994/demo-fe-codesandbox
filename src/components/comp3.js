a73f2d240bdd5b16ebe0d7001177b8c0eea81b4c
Tikay <kiet211194@gmail.com>
2020-05-08 19:29:21 +0700

    fix

--- src/components/comp3.js
+++ src/components/comp3.js
@@ -15,14 +15,17 @@ function comp3(props) {
       <div>
         <div className="flex transform transition-all duration-1000 hover:translate-x-full ease-in">
           <img
+            className="h-64 w-48 lg:h-full lg:w-full"
             src="https://codesandbox.io/static/10-bdc65e9d4799ce18b7ebcb2803b4cf19.png"
             alt="pic"
           ></img>
           <img
+            className="h-64 w-48 lg:h-full lg:w-full"
             src="https://codesandbox.io/static/2-f85edd5263e80fba8b193a48c53e2a2c.png"
             alt="pic"
           ></img>
           <img
+            className="h-64 w-48 lg:h-full lg:w-full"
             src="https://codesandbox.io/static/7-1176c2cecca6f46b34bd06cceaf62191.png"
             alt="pic"
           ></img>
